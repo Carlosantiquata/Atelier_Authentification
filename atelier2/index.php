be87fc251fcd5c385aa31e02315f0e85644b1f3e
Carlosantiquata <a.terrao-beca@cfacampusmontsouris.fr>
2025-11-05 10:20:29 +0100

    Update index.php

--- atelier2/index.php
+++ atelier2/index.php
@@ -6,8 +6,8 @@ session_start();
 // Si l'utilisateur possède déjà ce cookie, il sera redirigé automatiquement vers la page home.php
 // Dans le cas contraire il devra s'identifier.
 
-$eljeton = bin2hex(random_bytes(16));
-if (isset($_COOKIE['authToken']) && $_COOKIE['authToken'] === $eljeton) {
+
+if (isset($_COOKIE['authToken']) {
     header('Location: page_admin.php');
     exit();
 }
@@ -20,6 +20,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Vérification simple du username et de son password.
     // Si ok alors on initialise le cookie sur le poste de l'utilisateur 
     if ($username === 'admin' && $password === 'secret') {
+        $eljeton = bin2hex(random_bytes(16));
         setcookie('authToken', $eljeton, time() + 60, '/', '', false, true); // Le Cookie est initialisé et valable pendant 1 heure (3600 secondes) 
         header('Location: page_admin.php'); // L'utilisateur est dirigé vers la page home.php
         exit();
